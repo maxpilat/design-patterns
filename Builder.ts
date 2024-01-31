8080691e15b2a10ba90607d53c6d53c2737e2c05
MaksimPilat <pilatmd@outlook.com>
2024-01-31 12:59:43 +0300

    new file:   Builder.ts

---
+++ Builder.ts
@@ -0,0 +1,37 @@
+namespace Bulder {
+  class User {
+    name: string;
+    age!: number;
+    city!: string;
+
+    constructor(name: string) {
+      this.name = name;
+    }
+  }
+
+  class UserBuilder {
+    user: User;
+
+    constructor(name: string) {
+      this.user = new User(name);
+    }
+
+    setCity(city: string) {
+      this.user.city = city;
+      return this;
+    }
+
+    setAge(age: number) {
+      this.user.age = age;
+      return this;
+    }
+
+    build() {
+      return this.user;
+    }
+  }
+
+  const user = new UserBuilder("Alex").setAge(24).setCity("London").build();
+
+  console.log(user);
+}
