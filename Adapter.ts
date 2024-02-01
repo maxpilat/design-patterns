30537071866c75d96ee3d071885f1a32444ae821
MaksimPilat <pilatmd@outlook.com>
2024-02-01 10:06:54 +0300

    Adapter

---
+++ Adapter.ts
@@ -0,0 +1,41 @@
+namespace Adapter {
+  interface Engine {
+    simpleStart: () => void;
+  }
+
+  class EngineV4 implements Engine {
+    simpleStart() {
+      console.log('V4 started');
+    }
+  }
+
+  class EngineV8 {
+    complicatedStart() {
+      console.log('V8 started');
+    }
+  }
+
+  class EngineV8Adapter implements Engine {
+    private engine: EngineV4;
+
+    constructor(engine: EngineV4) {
+      this.engine = engine;
+    }
+    simpleStart() {
+      this.engine.simpleStart();
+    }
+  }
+
+  function clientCode(engine: Engine) {
+    engine.simpleStart();
+  }
+
+  const oldEngine = new EngineV4();
+  clientCode(oldEngine); // OK
+
+  const newEngine = new EngineV8();
+  // clientCode(newEngine); Error
+
+  const adapter = new EngineV8Adapter(oldEngine);
+  clientCode(adapter); // OK
+}
