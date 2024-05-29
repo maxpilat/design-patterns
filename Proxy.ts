95f385c5782cd7b9183d27139f63c6e182632eed
MaksimPilat <pilatmd@outlook.com>
2024-05-29 15:22:29 +0300

    new file:   Proxy.ts

---
+++ Proxy.ts
@@ -0,0 +1,30 @@
+namespace ProxyPattern {
+  interface IService {
+    request(): void;
+  }
+
+  class OriginalService implements IService {
+    request() {
+      console.log('OriginalService: Handling request');
+    }
+  }
+
+  class ProxyService implements IService {
+    constructor(private service: IService) {}
+
+    private checkAccess() {
+      console.log('ProxyService: Checking access');
+      return true;
+    }
+
+    request() {
+      if (this.checkAccess()) this.service.request();
+    }
+  }
+
+  const service = new OriginalService();
+
+  const proxy = new ProxyService(service);
+
+  proxy.request();
+}
